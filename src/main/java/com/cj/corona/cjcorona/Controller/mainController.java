50e4e6b65756a0e3aa2153fd0d1fd71dbcfc354e
kwangminini <rhkdals7362@gmail.com>
2020-08-26 03:08:45 +0900

    create Layout

--- src/main/java/com/cj/corona/cjcorona/Controller/mainController.java
+++ src/main/java/com/cj/corona/cjcorona/Controller/mainController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @Controller
 public class mainController {
-    @GetMapping("/hello")
+    @GetMapping("/mainBoard")
     public String main(Model model){
         return "mainBoard";
     }
